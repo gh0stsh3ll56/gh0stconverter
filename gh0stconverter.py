085b930833a319c3dfbee5b7fedc70e9169385ed
gh0stsh3ll5619 <139743805+gh0stsh3ll56@users.noreply.github.com>
2023-08-31 02:17:34 -0400

    Update gh0stconverter.py
    update to grab images

--- gh0stconverter.py
+++ gh0stconverter.py
@@ -3,21 +3,25 @@ import pdfplumber
 from docx import Document
 from PIL import Image
 import io
+from pdf2image import convert_from_path
+
+
+def sanitize_filename(filename):
+    return ''.join(c if c.isalnum() else '_' for c in filename)
 
 def pdf_to_markdown(pdf_path, output_dir):
-    markdown_content = ''
+    os.makedirs(output_dir, exist_ok=True)
+    img_subdir = os.path.join(output_dir, "images")
+    os.makedirs(img_subdir, exist_ok=True)
+    markdown_content = ""
+
     with pdfplumber.open(pdf_path) as pdf:
-        for page_num, page in enumerate(pdf.pages, start=1):
-            text = page.extract_text()
-            markdown_content += text + '\n\n'
-            
-            images = page.images
-            for img_num, image in enumerate(images, start=1):
-                img = pdf.pages[page_num - 1].to_image(resolution=150)
-                img_data = img.original
-                img_filename = f"{output_dir}/image_{page_num}_{img_num}.png"
-                img.save(img_filename)
-                markdown_content += f"![Image]({img_filename})\n\n"
+        for page_number, page in enumerate(pdf.pages):
+            img = page.to_image()
+            img_path = os.path.join(img_subdir, f"image_{page_number+1}.png")
+            img.save(img_path)
+            markdown_content += f"![Image {page_number+1}]({img_path})\n"
+
     return markdown_content
 
 def docx_to_markdown(docx_path, output_dir):
@@ -25,48 +29,51 @@ def docx_to_markdown(docx_path, output_dir):
     markdown_content = ''
     for para in doc.paragraphs:
         markdown_content += para.text + '\n\n'
-        
+
     for rel in doc.part.rels:
         if "image" in doc.part.rels[rel].target_ref:
             img_part = doc.part.related_parts[doc.part.rels[rel].target_ref]
             img_data = img_part.blob
             img = Image.open(io.BytesIO(img_data))
-            img_filename = f"{output_dir}/{os.path.basename(img_part.target_ref)}"
+            img_filename = os.path.join(output_dir, os.path.basename(img_part.target_ref))
             img.save(img_filename, format="PNG")
             markdown_content += f"![Image]({img_filename})\n\n"
-    
+
     return markdown_content
 
-def convert_to_markdown(input_path):
-    _, filename = os.path.split(input_path)
-    filename_without_extension, _ = os.path.splitext(filename)
-    output_dir = filename_without_extension
-    os.makedirs(output_dir, exist_ok=True)  # Create output directory if it doesn't exist
-    
-    markdown_content = ''
-    
-    _, extension = os.path.splitext(input_path)
-    
-    if extension == '.pdf':
-        markdown_content = pdf_to_markdown(input_path, output_dir)
-    elif extension == '.docx':
-        markdown_content = docx_to_markdown(input_path, output_dir)
+def convert_to_markdown(file_path):
+    if os.path.isfile(file_path):
+        markdown_content = pdf_to_markdown(file_path, "markdowns")
+        output_file_path = os.path.join("markdowns", os.path.basename(file_path) + ".md")
+        with open(output_file_path, "w", encoding="utf-8") as markdown_file:
+            markdown_file.write(markdown_content)
+    elif os.path.isdir(file_path):
+        for root, _, files in os.walk(file_path):
+            for file in files:
+                if file.lower().endswith(('.pdf', '.docx')):
+                    file_path = os.path.join(root, file)
+                    convert_to_markdown(file_path)
     else:
-        return "Unsupported file format."
-    
-    markdown_file_path = f"{output_dir}/{filename_without_extension}.md"
-    with open(markdown_file_path, 'w') as markdown_file:
-        markdown_file.write(markdown_content)
-        
-    return markdown_file_path
+        print("Invalid file or directory path.")
+
+
 
 if __name__ == "__main__":
-    input_path = input("Enter the path of the file to convert: ").strip("'\"")
-    
-    if not os.path.exists(input_path):
-        print("File not found.")
-    else:
-        input_path = os.path.abspath(input_path)
-        markdown_file_path = convert_to_markdown(input_path)
-        
-        print(f"Conversion complete. Markdown file saved at: {markdown_file_path}")
+    print(r"""
+
+
+             ('-. .-.            .-')    .-') _                              .-') _      (`-.      ('-.  _  .-')   .-') _     ('-.  _  .-')   
+            ( OO )  /           ( OO ). (  OO) )                            ( OO ) )   _(OO  )_  _(  OO)( \( -O ) (  OO) )  _(  OO)( \( -O )  
+  ,----.    ,--. ,--.  .----.  (_)---\_)/     '._  .-----.  .-'),-----. ,--./ ,--,',--(_/   ,. \(,------.,------. /     '._(,------.,------.  
+ '  .-./-') |  | |  | /  ..  \ /    _ | |'--...__)'  .--./ ( OO'  .-.  '|   \ |  |\\   \   /(__/ |  .---'|   /`. '|'--...__)|  .---'|   /`. ' 
+ |  |_( O- )|   .|  |.  /  \  .\  :` `. '--.  .--'|  |('-. /   |  | |  ||    \|  | )\   \ /   /  |  |    |  /  | |'--.  .--'|  |    |  /  | | 
+ |  | .--, \|       ||  |  '  | '..`''.)   |  |  /_) |OO  )\_) |  |\|  ||  .     |/  \   '   /, (|  '--. |  |_.' |   |  |  (|  '--. |  |_.' | 
+(|  | '. (_/|  .-.  |'  \  /  '.-._)   \   |  |  ||  |`-'|   \ |  | |  ||  |\    |    \     /__) |  .--' |  .  '.'   |  |   |  .--' |  .  '.' 
+ |  '--'  | |  | |  | \  `'  / \       /   |  | (_'  '--'\    `'  '-'  '|  | \   |     \   /     |  `---.|  |\  \    |  |   |  `---.|  |\  \  
+  `------'  `--' `--'  `---''   `-----'    `--'    `-----'      `-----' `--'  `--'      `-'      `------'`--' '--'   `--'   `------'`--' '--' 
+
+
+    """)
+    input_path = input("Enter the path of the file or directory to convert: ")
+    result = convert_to_markdown(input_path)
+    print(result)
