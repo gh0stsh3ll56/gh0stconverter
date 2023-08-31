d6f61b8dd86dddeaee28138f7a81c976b86dd36c
gh0stsh3ll5619 <139743805+gh0stsh3ll56@users.noreply.github.com>
2023-08-31 02:46:45 -0400

    Update gh0stconverter.py

--- gh0stconverter.py
+++ gh0stconverter.py
@@ -5,7 +5,6 @@ from PIL import Image
 import io
 from pdf2image import convert_from_path
 
-
 def sanitize_filename(filename):
     return ''.join(c if c.isalnum() else '_' for c in filename)
 
@@ -21,6 +20,8 @@ def pdf_to_markdown(pdf_path, output_dir):
             img_path = os.path.join(img_subdir, f"image_{page_number+1}.png")
             img.save(img_path)
             markdown_content += f"![Image {page_number+1}]({img_path})\n"
+            page_text = page.extract_text()  # Extract text from the page
+            markdown_content += f"{page_text}\n\n"  # Add extracted text content
 
     return markdown_content
 
@@ -38,23 +39,37 @@ def docx_to_markdown(docx_path, output_dir):
             img_filename = os.path.join(output_dir, os.path.basename(img_part.target_ref))
             img.save(img_filename, format="PNG")
             markdown_content += f"![Image]({img_filename})\n\n"
-
+    
     return markdown_content
 
 def convert_to_markdown(file_path):
     if os.path.isfile(file_path):
-        markdown_content = pdf_to_markdown(file_path, "markdowns")
-        output_file_path = os.path.join("markdowns", os.path.basename(file_path) + ".md")
-        with open(output_file_path, "w", encoding="utf-8") as markdown_file:
+        filename_without_extension = os.path.splitext(os.path.basename(file_path))[0]
+        short_filename = sanitize_filename(filename_without_extension)[:15]  # Taking first 15 characters
+        output_dir = os.path.join("markdowns", short_filename)
+        os.makedirs(output_dir, exist_ok=True)
+        
+        markdown_content = ""
+        if file_path.lower().endswith('.pdf'):
+            markdown_content = pdf_to_markdown(file_path, output_dir)
+        elif file_path.lower().endswith('.docx'):
+            markdown_content = docx_to_markdown(file_path, output_dir)
+        
+        markdown_file_path = os.path.join(output_dir, f"{short_filename}.md")
+        
+        with open(markdown_file_path, 'w', encoding='utf-8') as markdown_file:
             markdown_file.write(markdown_content)
+        
+        return f"Conversion complete. Markdown file saved at: {markdown_file_path}"
+    
     elif os.path.isdir(file_path):
         for root, _, files in os.walk(file_path):
             for file in files:
                 if file.lower().endswith(('.pdf', '.docx')):
                     file_path = os.path.join(root, file)
                     convert_to_markdown(file_path)
-    else:
-        print("Invalid file or directory path.")
+        
+        return f"Conversion complete for all files in the directory: {file_path}"
 
 
 
